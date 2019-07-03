70ecc616a7503a61cb24bd5fbaa04c8095ae5391
neptune2359 <neptune2359@gmail.com>
2019-07-03 07:08:19 +0100

    Changes to java files

--- ebay-technical-assignment/src/com/assignment/OldestPerson.java
+++ ebay-technical-assignment/src/com/assignment/OldestPerson.java
@@ -1,10 +1,22 @@
 package com.assignment;
 
-public class OldestPerson {
+import java.time.*;
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class OldestPerson { 
+	
+   public static void main(String[] args)
+    {
+        // date of birth
+        LocalDate pdate = LocalDate.of(1989, 04, 11);
+        // current date
+        LocalDate now = LocalDate.now();
+        // difference between current date and date of birth
+        Period diff = Period.between(pdate, now);
+ 
+     System.out.printf("\nI am  %d years, %d months and %d days old.\n\n", 
+                    diff.getYears(), diff.getMonths(), diff.getDays());
+     
+     System.out.print(diff.getDays());
+   }
 }
